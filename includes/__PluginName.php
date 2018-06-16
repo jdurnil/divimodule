20fffe27f39d8ee64124cae7adce8e8fa7e26714
jdurnil <jdurnil@gmail.com>
2018-06-16 09:48:15 -0400

    Initial commit from Create React App

---
+++ includes/__PluginName.php
@@ -0,0 +1,46 @@
+<?php
+
+class DGR_Divigrid extends DiviExtension {
+
+	/**
+	 * The gettext domain for the extension's translations.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $gettext_domain = 'dgr-divigrid';
+
+	/**
+	 * The extension's WP Plugin name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $name = 'divigrid';
+
+	/**
+	 * The extension's version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * DGR_Divigrid constructor.
+	 *
+	 * @param string $name
+	 * @param array  $args
+	 */
+	public function __construct( $name = 'divigrid', $args = array() ) {
+		$this->plugin_dir     = plugin_dir_path( __FILE__ );
+		$this->plugin_dir_url = plugin_dir_url( $this->plugin_dir );
+
+		parent::__construct( $name, $args );
+	}
+}
+
+new DGR_Divigrid;
